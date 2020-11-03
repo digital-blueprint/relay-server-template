79dc69d7ef886d2b2d12cb10a06fe6544956d7d2
Christoph Reiter <reiter.christoph@gmail.com>
2020-11-03 13:42:36 +0100

    Make the linter happy again.

--- src/MyCustomPersonProvider.php
+++ src/MyCustomPersonProvider.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App;
 
@@ -8,29 +9,33 @@ use DBP\API\CoreBundle\Service\PersonProviderInterface;
 
 class MyCustomPersonProvider implements PersonProviderInterface
 {
-
     public function getPersons(array $filters): array
     {
         // TODO: Implement getPersons() method.
+        return [];
     }
 
     public function getPersonsByNameAndBirthday(string $givenName, string $familyName, \DateTime $birthDay): array
     {
         // TODO: Implement getPersonsByNameAndBirthday() method.
+        return [];
     }
 
     public function getPerson(string $id, bool $full): Person
     {
         // TODO: Implement getPerson() method.
+        return new Person();
     }
 
     public function getCurrentPerson(): Person
     {
         // TODO: Implement getCurrentPerson() method.
+        return new Person();
     }
 
     public function getPersonForExternalService(string $service, string $serviceID): Person
     {
         // TODO: Implement getPersonForExternalService() method.
+        return new Person();
     }
 }
