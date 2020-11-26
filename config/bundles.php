f2fcb2f1f62cfd454b2f24e4db7f4f9785e73634
Christoph Reiter <reiter.christoph@gmail.com>
2020-11-26 12:45:31 +0100

    Update psalm

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => ($_ENV['DISABLE_DEV_SERVER'] ?? 'true') !== 'true'],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => false],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
