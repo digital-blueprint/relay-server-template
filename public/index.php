617ded69398edf00a6519b924ddf0aff339127c0
Christoph Reiter <reiter.christoph@gmail.com>
2021-05-04 13:57:41 +0200

    Re-run symfony recipes to get updated bootstrap/kernel/console scripts
    composer recipes:install symfony/console --force
    composer recipes:install symfony/framework-bundle --force
    
    This gets rid of a few deprecation warnings

--- public/index.php
+++ public/index.php
@@ -3,31 +3,18 @@
 declare(strict_types=1);
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-// FPM renames all environment variables!
-if (isset($_SERVER['REDIRECT_APP_ENV'])) {
-    $_SERVER['APP_ENV'] = $_SERVER['REDIRECT_APP_ENV'];
-}
+require dirname(__DIR__).'/vendor/autoload.php';
 
-require dirname(__DIR__).'/config/bootstrap.php';
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     Debug::enable();
-} else {
-    // Set a dummy dumper handler to avoid left over dump() commands breaking production
-    \Symfony\Component\VarDumper\VarDumper::setHandler(function ($var) {});
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
