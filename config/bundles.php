6414bd07920a613456110d6a61c4624923bb2664
Christoph Reiter <reiter.christoph@gmail.com>
2021-07-26 15:37:25 +0200

    Integrate the new keycloak bundle
    This mainly means the keycloak config moves to a separate bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    DBP\API\KeycloakBundle\DbpKeycloakBundle::class => ['all' => true],
     DBP\API\CoreBundle\DbpCoreBundle::class => ['all' => true],
 ];
