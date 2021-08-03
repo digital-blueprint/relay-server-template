05a756a9df925696d13585e8430e42ffd3134355
Christoph Reiter <reiter.christoph@gmail.com>
2021-08-03 11:44:08 +0200

    Switch to the new dbp package names

--- config/bundles.php
+++ config/bundles.php
@@ -1,17 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => false],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    DBP\API\KeycloakBundle\DbpKeycloakBundle::class => ['all' => true],
-    DBP\API\CoreBundle\DbpCoreBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Dbp\Relay\KeycloakBundle\DbpRelayKeycloakBundle::class => ['all' => true],
+    Dbp\Relay\CoreBundle\DbpRelayCoreBundle::class => ['all' => true],
 ];
