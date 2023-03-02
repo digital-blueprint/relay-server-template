bbd7bdfb00f85061e2f7dc3410fad51c6e11d6e7
Manuel Kocher <manuel.kocher@tugraz.at>
2023-03-02 10:58:21 +0100

    Set UTC as timezone as early as possible

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,6 +7,9 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+// XXX: some of our bundles depend on UTC being the timezone, so set it early
+date_default_timezone_set('UTC');
+
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
