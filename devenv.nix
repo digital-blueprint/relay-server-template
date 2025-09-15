dcb4c20b9b487cc803aee5e3b24dc7ecba771891
Patrizio Bekerle <patrizio@bekerle.com>
2025-09-15 14:04:16 +0200

    devenv: add more tooling
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, ... }:
 
 {
   # https://devenv.sh/supported-languages/php/
@@ -7,9 +7,43 @@
     version = "8.3";
   };
 
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    just
+    zellij
+  ];
+
   enterShell = ''
     echo "🔨 DBP Relay API Server Template dev shell"
+    echo "🐘 PHP version: $(php --version | head -n 1)"
   '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    # https://devenv.sh/reference/options/#git-hookshookstreefmt
+    # https://github.com/numtide/treefmt
+    # https://github.com/numtide/treefmt-nix
+    treefmt = {
+      enable = true;
+      settings.formatters = with pkgs; [
+        nodePackages.prettier
+        shfmt
+        nixfmt-rfc-style
+        statix
+        taplo
+        php83Packages.php-cs-fixer
+      ];
+    };
+
+    # https://devenv.sh/reference/options/#git-hookshooksdeadnix
+    # https://github.com/astro/deadnix
+    deadnix = {
+      enable = true;
+      settings = {
+        edit = true; # Allow to edit the file if it is not formatted
+      };
+    };
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
---
+++ justfile
@@ -0,0 +1,33 @@
+# Use `just <recipe>` to run a recipe
+# https://just.systems/man/en/
+
+# By default, run the `--list` command
+default:
+    @just --list
+
+# Aliases
+
+alias fmt := format
+
+# Format all files
+[group('linter')]
+format args='':
+    treefmt {{ args }}
+
+# Format all files using pre-commit
+[group('linter')]
+format-all args='':
+    composer install
+    pre-commit run --all-files {{ args }}
+
+# Run linters on all files
+[group('linter')]
+lint:
+    nix-shell -p php83Packages.composer --run "composer run lint"
+
+# Add git commit hashes to the .git-blame-ignore-revs file
+[group('linter')]
+add-git-blame-ignore-revs:
+    git log --pretty=format:"%H" --grep="^lint" >> .git-blame-ignore-revs
+    sort .git-blame-ignore-revs | uniq > .git-blame-ignore-revs.tmp
+    mv .git-blame-ignore-revs.tmp .git-blame-ignore-revs
